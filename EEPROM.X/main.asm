be74d69d1f63e35e8e8fcb5974a1bb626f5dd951
Bruno Lima <bg.lima@hotmail.com>
2016-09-23 16:49:00 -0300

    Added swap function between address val1 and val2

--- EEPROM.X/main.asm
+++ EEPROM.X/main.asm
@@ -10,23 +10,26 @@
   
 	; Pre-writing data to EE memory, for test purporses
 	ORG	0x2100
-	DE	0x0A, "HelloWorld"
+	DE	0x0A, "123456789A"
  
 	; Start of code
 	ORG	0x0000
 	
 ; HEAPIFY FUNCTION VARIABLES
+cInd	EQU	0x2B	; Current index of element
 cAdd	EQU	0x20	; Current address of element
 lAdd	EQU	0x21	; LeftC	  address of element
 rAdd	EQU	0x22	; RightC  address of element
-larger	EQU	0x23	; Largest index element
+larger	EQU	0x23	; Largest address of element
 	
 ; BUILD MAX HEAP FUNCTION VARIABLES
+iniHAdd	EQU	0x2C
 index	EQU	0x24	; Current element index
 size	EQU	0x25	; Size of Heap
 
 ; SWAP FUNCTION VARIABLES
-temp	EQU	0x26	; Temporary variable
+temp1	EQU	0x26	; Temporary variable
+temp2	EQU	0x2D
 
 ; GREAT FUNCTION VARIABLES
 val1	EQU	0x27
@@ -42,6 +45,18 @@ clearf:	; Function that clears flags Z and C from STATUS
 	BCF STATUS, Z
 	BCF STATUS, C
 	RETURN
+	
+diff:	; Function that returns if val1 and value2 are equal
+	; Uses registers val1 and val2 (as input) and register W (as output)
+	CALL	clearf	    ; Clear flags before use it
+	MOVF	val1, W
+	SUBWF	val2, W
+	BTFSC	STATUS, Z
+	RETLW	0x01	    ; Values are equal
+	BTFSC	STATUS, C
+	RETLW	0x00	    ; Value2 is greater
+	; val1IsGreater
+	RETLW	0x00
 	
 great:	; Function that returns the greatest value among "cInd", "lInd" and "rInd"
 	; Uses registers val1 and val2 (as input) and register W (as output)
@@ -62,18 +77,157 @@ buildMH: ; buildMaxHeap function. Starts at node size/2 and goes back until it
 	MOVWF	index
 	BCF	index, 0    ; Clear LSB so that rotate does not set STATUS, C
 	CALL	clearf	    ; Clear possible C flags
-	RRF	index, 1    ; Rotate index to right (divide by two). Store result in index
+	RRF	index, 0    ; Rotate index to right (divide by two)
+	MOVWF	index
 	
 loopBMH: ; Main loop of buildMaxHeap
-    
+	MOVF	index, W
+	MOVWF	cInd
+	CALL	heapify
+	CALL	clearf
+	
 	DECFSZ	index, 1    ; If index is zero, stop.
 	GOTO	loopBMH
 	RETURN
 
 heapify: ; Function that check which element is greater ammong addresses 
 	 ; cAdd, rAdd and lAdd. Return the address of larger element
+	MOVWF	cInd	    ; Storing current index
+	MOVWF	cAdd	    ; Moving current index to cAdd
+	
+	
+	ADDWF	iniHAdd, W
+	MOVWF	cAdd	    ; Now, cAdd contains the address of cInd element
+
+loopHY	; Left child
+	CALL	clearf
+	
+	MOVF	cInd, W
+	MOVWF	cAdd
+	MOVWF	lAdd	    ; Left child
+	MOVWF	rAdd	    ; Right child
+	
+	; Initiate cInd as larger
+	MOVF	iniHAdd, W
+	ADDWF	cAdd, 1
+	MOVF	cAdd, W
+	MOVWF	larger	    ; Address of larger number initiate as cAdd
+	
+	; Calculating left child address
+	BCF	lAdd, 7	    ; Clean MSB so that rotate does not set STATUS, C
+	RLF	lAdd, 1	    ; Rotate rAdd to left and store on its own
+	; if lAdd (stil an index of leftChild)  is greater than size, stop
+	MOVF	lAdd, W
+	MOVWF	val1
+	MOVF	size, W
+	MOVWF	val2
+	CALL	great
+	BTFSC	W, 0   ; If W is equal to 1, return from here
+	RETURN
+	; Keep calculating address
+	MOVF	iniHAdd, W
+	ADDWF	lAdd, 1	    ; Calculating lChild address
+	
+	; Compare element of cAdd with element lAdd
+	MOVF	cAdd, W	    ; val1
+	MOVWF	FSR
+	MOVF	INDF, W
+	MOVWF	val1	    
+	MOVF	lAdd, W	    ; val2
+	MOVWF	FSR
+	MOVF	INDF, W
+	MOVWF	val2
+	call	great  
+	BTFSS	W, 0
+	GOTO	lNotLarger
+	
+	MOVF	larger, W   ; If got here, lChild is larger
+	MOVWF	FSR
+	MOVF	val2, W
+	MOVWF	INDF
+
+lNotLarger:
+	; Right child address
+	BCF	rAdd, 7
+	RLF	rAdd, 1
+	INCF	rAdd, 1
+	
+
+	MOVF	iniHAdd, W
+	ADDWF	rAdd, 1	    ; Calculating rChild address
+	; check if rAdd (stil an index of rightChild)  is greater than size
+	MOVF	rAdd, W
+	MOVWF	val1
+	MOVF	size, W
+	MOVWF	val2
+	CALL	great
+	BTFSC	W, 0   ; If W is equal to 1, return from here
+	GOTO	compareLarger
+	
+	; Compare element of rChild with element of Larger add
+	MOVF	larger, W	    ; val1
+	MOVWF	FSR
+	MOVF	INDF, W
+	MOVWF	val1	    
+	MOVF	rAdd, W	    ; val2
+	MOVWF	FSR
+	MOVF	INDF, W
+	MOVWF	val2
+	call	great  
+	BTFSS	W, 0
+	GOTO	compareLarger
+	
+	MOVF	larger, W   ; If got here, lChild is larger
+	MOVWF	FSR
+	MOVF	val2, W
+	MOVWF	INDF
+	
+
+compareLarger:
+	; Compare if larger address is different than cAdd
+	MOVF	larger, W
+	MOVWF	val1
+	MOVF	cAdd, W
+	MOVWF	val2
+	CALL	diff
+	BTFSS	W, 0
 	RETURN
 	
+largAndCAddDifferent:
+	MOVF	larger, W
+	MOVWF	val1
+	MOVF	cAdd, W
+	MOVWF	val2
+	CALL	swap
+
+; Call Heapify at next element
+	MOVF	larger, W
+	SUBWF	iniHAdd, 0
+	MOVWF	cInd
+	GOTO	loopHY
+    
+	RETURN
+
+; val1 and val2 are the address of variables to be swapped
+swap:	
+	MOVF	val1, W
+	MOVWF	FSR
+	MOVF	INDF, W
+	MOVWF	temp1	; variable from val1 to temp	
+	
+	MOVF	val2, W
+	MOVWF	FSR
+	MOVF	INDF, W	
+	MOVWF	temp2	; variable from val2 to temp
+	
+	MOVF	temp1, W
+	MOVWF	INDF	; temp1 to val2
+	MOVF	val1, W
+	MOVWF	FSR
+	MOVF	temp2, W
+	MOVWF	INDF	; temp2 to val1
+	
+	RETURN
 
 rEEByte:		    ; Address to be read must be in W register. The result will override W.
 	BANKSEL	EEADR
@@ -112,11 +266,18 @@ setup:
 	MOVWF	INDF	; Moving value read to maxB address
 	CALL	rEEData
 
-	DECF	maxB, W	; Decrement one from maxB (due to control byte in heap) and store in W
+	;DECF	maxB, W	; Decrement one from maxB (due to control byte in heap) and store in W
+	MOVF	maxB, W
 	MOVWF	size	; Size of HEAP
 	
+	MOVLW	iniAdd
+	MOVWF	iniHAdd
+	DECF	iniHAdd, 1
+	
 	CALL buildMH
+	
 	NOP
+
 	
 loop:	
 	GOTO	loop
